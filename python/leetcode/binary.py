c6b3196451398b7e15ee3b4f42551539f9833565
zhaopengyun <zpy94254@gmail.com>
2020-04-15 09:33:13 +0800

    +

---
+++ python/leetcode/binary.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @time: 2020-04-15 08:40
+# @file: binary.py
+
+
+def hammingWeight(n):
+    """
+    面试题15. 二进制中1的个数
+    :type n: int
+    :rtype: int
+    """
+    res = 0
+    while n:
+        res += 1
+        n = n & (n - 1)
+    return res
+
+
--- python/leetcode/str.py
+++ python/leetcode/str.py
@@ -42,7 +42,43 @@ def reverse(x):
         return -ks
     return ks
 
+def romanToInt(s):
+    """
+    leetcode 13. 罗马数字转整数
+    :param s:
+    :return:
+    """
+    mp = {
+        "I":1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C":100,
+        "D":500,
+        "M":1000,
+        "IV":4,
+        "IX":9,
+        "XL":40,
+        "XC":90,
+        "CD":400,
+        "CM":900,
+    }
+    lens = len(s)
+    res = 0
+    i = 0
+    while i < lens:
+        if i+1 < lens and s[i:i+2] in mp:
+            res += mp[s[i:i+2]]
+            i += 2
+        else:
+            res += mp[s[i]]
+            i += 1
+    return res
+    
+
 if __name__ == '__main__':
     # print(longestCommonPrefix(["dog","racecar","car"]))
     
     print(reverse(0))
+    
+    print(romanToInt("MCMXCIV"))
